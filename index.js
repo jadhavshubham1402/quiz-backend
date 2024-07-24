b7620da60470d05c3f42771e8a7a5f9fbd484537
Shubham Jadhav <shubham.jadhav@code-b.dev>
2024-07-25 02:08:22 +0530

    index.js added

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
-const http = require('http');
-const app = require('./app');
-const port = process.env.PORT || 3000;
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const connectToDatabase = require("./database");
+const loginRoutes = require("./router/route");
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(
+  cors()
+);
 
-const server = http.createServer(app);
-server.listen(port,()=>{console.log('this app is running on '+port)});
+app.use(express.json())
+
+connectToDatabase.then(() => {
+  //health check routes
+  app.get("/", (req, res) => {
+    res.json("quiz backend is Running");
+  });
+
+  app.use("/api/", loginRoutes);
+
+  //error handler
+  app.use(function (err, req, res, next) {
+    res.status(500);
+    res.send({
+      message: "something went wrong",
+      error: err?.message || err,
+      code: err.code,
+    });
+  });
+
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is running at port ${process.env.PORT}`);
+  });
+});
+
+module.exports = app;
